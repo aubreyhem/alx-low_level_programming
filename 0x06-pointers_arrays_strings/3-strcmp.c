eecc9f621258d11b03082c2058f3b2a3873bba9c
Aubrey <aubreyhembley@hotmail.com>
2024-07-04 19:15:37 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,10 +3,16 @@
 /**
  * _strcmp - write a function that compares two strings
  *
- * @s1: input string
- * @s2: input string
+* @s1: string 1 input to compare
+ * @s2: against this other string 2
  *
- * Return: 0 if s1 and s2 are equal, if not return other number
+ * Return: 0 if s1 and s2 are equal
+ *        negative integer if the stopping character
+ *                in @s1 was less than the stopping
+ *                character in @s2
+ *        positive integer if the stopping character
+ *                in @s1 was greater than the stopping
+ *                character in @s2
  */
 
 int _strcmp(char *s1, char *s2);
