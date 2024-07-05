9f787bad7f0173e37018e3b096a9b0883dc71b6c
Aubrey <aubreyhembley@hotmail.com>
2024-07-04 18:37:28 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,23 +11,11 @@
 
 int _strcmp(char *s1, char *s2);
 {
-	int i = 0, r;
-
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
+		;
+	if (*s1 == *s2)
 	{
-
-		if (s1[i] != s2[i])
-		{
-
-			r = s1[i] - s2[i];
-			break;
-		}
-		else
-		{
-			r = s1[i] - s2[i];
-		}
-		i++;
+		return (0);
 	}
-
-	return (r);
+	return (*s1 - *s2);
 }
